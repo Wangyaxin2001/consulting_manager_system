9583532d7d176c323432876627fbd3999ddb68f4
Wangyaxin2001 <1009283095@qq.com>
2023-10-19 14:36:30 +0800

    本地提交

--- consulting_manager_system/cms/src/main/java/com/yaxin/cms/dao/CategoryDao.java
+++ consulting_manager_system/cms/src/main/java/com/yaxin/cms/dao/CategoryDao.java
@@ -19,6 +19,9 @@ public interface CategoryDao extends BaseMapper<Category> {
     //查询栏目表中最大的order_num值
     Integer getMaxOrderNum();
 
+    //查询父栏目下的子栏目
+    List<Category> queryByParentId(Integer parentId);
+
     //查询所有一级栏目（含二级栏目s）
     List<CategoryExtend> queryAllWithCates();
 
